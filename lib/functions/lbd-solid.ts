256f4e8450fdb865a4775f476622a371b641da89
jmauwerb <jeroen.werbrouck@ugent.be>
2021-01-26 17:58:19 +0100

    initiating functions to work with solid community server

--- lib/functions/lbd-solid.ts
+++ lib/functions/lbd-solid.ts
@@ -1,5 +1,8 @@
 import fetch from 'node-fetch'
 import v4 from 'uuid'
+const util = require('util');
+const validate = require('turtle-validator')
+const mime = require('mime-types')
 
 async function createProject(metadata, session) {
     try {
@@ -24,20 +27,71 @@ async function createProject(metadata, session) {
     }
 }
 
-async function createGraph() {
+async function uploadGraph(url, data, session): Promise<void> {
     try {
         // check if graph does not exist yet
-        // await checkExistence()
+        const exists = await checkExistence(url)
+        if (exists) {
+            throw new Error("Resource already exists")
+        }
 
+        // check if data is turtle
+        await validateTTL(data)
+
+        var requestOptions = {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "text/turtle"
+        },
+        body: data.toString(),
+        redirect: 'follow'
+        };
+
+        const response = await fetch(url, requestOptions)
+        const text = await response.text()
+
+        return
     } catch (error) {
         error.message = `Could not create graph - ${error.message}`
         throw error
     }
 }
 
-async function createMetadataGraph() {
+async function uploadDocument(url, data, session): Promise<void> {
     try {
+
+        //content-type is guessed by url (default: text/plain)
+        let mimetype = mime.lookup(url)
+        if (mimetype === false) {
+            mimetype = "text/plain"
+        }
+
+        var requestOptions = {
+          method: 'PUT',
+          headers: {
+            "Content-Type": mimetype
+          },
+          body: data,
+          redirect: 'follow'
+        };
         
+        fetch(url, requestOptions)
+          .then(response => response.text())
+          .then(result => console.log(result))
+          .catch(error => console.log('error', error));
+    } catch (error) {
+        error.message = `Could not upload document - ${error.message}`
+        throw error
+    }
+}
+
+async function uploadMetadataGraph(url, data, session): Promise<void> {
+    try {
+        if (!url.endsWith(".meta")) {
+            url = url.concat('.meta')
+        }
+        const response = await uploadGraph(url, data, session)
+        return response
     } catch (error) {
         error.message = `Could not create metadata graph - ${error.message}`
         throw error
@@ -107,6 +161,22 @@ async function checkExistence(graph): Promise<boolean> {
         throw error    }
 }
 
+function validateTTL(data) {
+    return new Promise<void>((resolve, reject) => {
+        validate(data, (fb) => {
+            if (fb.warnings.length === 0 && fb.errors.length === 0) {
+                resolve()
+            } else {
+                reject(fb)
+            }
+        })
+    })
+}
+
 // getLbdLocation({webId: "http://jwerbrouck.consolidproject.be/profile/card#me"})
 
-checkExistence("https://jwerbrouck.inrupt.net/profile/card#me")
+// checkExistence("https://jwerbrouck.inrupt.net/profile/card#me")
+        // var fs = require('fs');
+        // var data = fs.readFileSync('/home/jmauwerb/Pictures/5fav.JPG');
+        // var file = Buffer.from(data);
+        // uploadDocument("http://localhost:3000/qefmoizqj/zeeiee.jpg", file, null)
