fd8f0b02deb861b5c112dd4881d00b39baa34b31
jmauwerb <jeroen.werbrouck@ugent.be>
2021-02-03 12:36:56 +0100

    publishing new version on npm

--- jsdoc_generate.js
+++ jsdoc_generate.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 const readmeText = `# LBDserver API
 
-Documentation for the LBDserver project. Includes shared interfaces as well as functions for communicating with the LBDserver backend.
+Documentation for the LBDserver project running on the Solid Community Server. LBDserver is a project for managing Linked Building Data, using both RDF and non-RDF resources. To make the LBDserver infrastructure federated, we base upon on the (WIP) [Community Solid Server prototype](https://github.com/solid/community-server). Note that both the CSS as this API functions are still higly experimental. The API is used, amongst others, in the [LBDserver frontend conSOlid prototype](https://github.com/LBDserver/front-react/tree/consolid-front).
 
 ## Installation
 
